67f4365cd1d83add80b8cae14905544cfe336f56
Yang Su <suyang1+services@gmail.com>
2011-11-12 15:38:29 -0500

    added testing script

---
+++ test.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+BQOUT="bq.out"
+STLQOUT="stlq.out"
+
+g++ queue.cpp -c -o queue.a
+
+g++ queue.a storagetest.cpp -o storage
+echo "timing storage :"
+time ./storage
+
+g++ queue.a correctnesstest.cpp -o correctness
+echo "testing correctness :"
+for i in {0..20}; do
+  echo "queue bound size "$i
+  ./correctness $i $BQOUT $STLQOUT
+  diff $BQOUT $STLQOUT
+done
+
+rm queue.a storage correctness
