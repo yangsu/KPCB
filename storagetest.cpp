b1b1fbbe7ddb220a076f6c7191020c235edccb71
Yang Su <suyang1+services@gmail.com>
2011-11-12 14:56:03 -0500

    removed some print statements in storagetest

--- storagetest.cpp
+++ storagetest.cpp
@@ -15,8 +15,6 @@ int main(int argc, char const *argv[]) {
   int result;
   for (int i = 0; i < 60000000; ++i) {
     success = q.dequeue(&result);
-    if (i % 10000000 == 0)
-      printf("%d\n", result);
     count += (success) ? 1 : 0;
   }
   printf("dequeued %d\n", count);
