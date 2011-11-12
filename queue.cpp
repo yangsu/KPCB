a7f115e8ceb52b69fe6a07895cba911290501760
Yang Su <suyang1+services@gmail.com>
2011-11-12 14:42:48 -0500

    fixed head/tail pointer bug. head and tail now wrap around block during enqueue and dequeue

--- queue.cpp
+++ queue.cpp
@@ -2,11 +2,13 @@
 #include <string.h>
 #include "queue.h"
 
+#define INITIAL_SIZE 1
+
 BoundedQueue::BoundedQueue(int n) {
   bound = n;
   size = 0;
   
-  base = (int*)malloc(sizeof(int));
+  base = (int*)malloc(INITIAL_SIZE*sizeof(int));
   head = base;
   tail = base;
   blockSize = 1;
@@ -19,17 +21,25 @@ BoundedQueue::~BoundedQueue() {
 bool BoundedQueue::enqueue(int n) {
   if (size < bound) {
     if (size == blockSize) {
-      int doublesize = blockSize * 2;
+      int doublesize = blockSize << 1;
       blockSize = (doublesize > bound) ? bound : doublesize;
       int* temp = (int*)malloc(blockSize*sizeof(int));
-      memcpy(temp, head, size*sizeof(int));
+      
+      if (tail >= head)
+        memcpy(temp, head, size*sizeof(int));
+      else {
+        int c1 = base + blockSize - head;
+        int c2 = size - c1;
+        memcpy(temp, head, c1*sizeof(int));
+        memcpy(temp + c1, tail, c2*sizeof(int));
+      }
       free(base);
       base = temp;
       head = base;
       tail = base + size;
     }
     *tail = n;
-    ++tail;
+    tail = (tail >= base + blockSize - 1) ? base : tail + 1;
     ++size;
     return true;
   }
@@ -39,7 +49,7 @@ bool BoundedQueue::enqueue(int n) {
 bool BoundedQueue::dequeue(int* ptr) {
   if (size > 0) {
     *ptr = *head;
-    ++head;
+    head = (head >= base + blockSize - 1) ? base : head + 1;
     --size;
     return true;
   }
